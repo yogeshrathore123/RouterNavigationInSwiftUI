13d747c48fa79f5f2c4be20a9e6d00a97f9e79bc
Yogesh Rathore <yogeshrathore251@gmail.com>
2024-12-30 14:00:21 +0530

    initial commit

---
+++ RouterNavigationInSwiftUI/RouterNavigationInSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RouterNavigationInSwiftUI
+//
+//  Created by Yogesh Rathore on 30/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RouterNavigationInSwiftUI/RouterNavigationInSwiftUI/RouterNavigationInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterNavigationInSwiftUIApp.swift
+//  RouterNavigationInSwiftUI
+//
+//  Created by Yogesh Rathore on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RouterNavigationInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
